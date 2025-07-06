26377a71ae58c7b954a91598e55c26a665c95096
Yanneck <yanneck.haendeler@timetoact.de>
2025-07-06 10:10:24 +0200

    chore: format

--- src/Core/Camera.cpp
+++ src/Core/Camera.cpp
@@ -4,15 +4,14 @@
 #include <iostream>
 
 // Constructor and other methods from your file remain the same...
-Camera::Camera(GLFWwindow* window, glm::vec3 position) :
-    m_Window(window),
-    m_Position(position),
-    m_WorldUp(0.0f, 1.0f, 0.0f),
-    m_Yaw(-90.0f),
-    m_Pitch(-10.0f),
-    m_Zoom(45.0f),
-    m_MovementSpeed(5.0f),
-    m_AspectRatio(16.0f / 9.0f)
+Camera::Camera(GLFWwindow *window, glm::vec3 position) : m_Window(window),
+                                                         m_Position(position),
+                                                         m_WorldUp(0.0f, 1.0f, 0.0f),
+                                                         m_Yaw(-90.0f),
+                                                         m_Pitch(-10.0f),
+                                                         m_Zoom(45.0f),
+                                                         m_MovementSpeed(5.0f),
+                                                         m_AspectRatio(16.0f / 9.0f)
 {
     double xpos, ypos;
     glfwGetCursorPos(m_Window, &xpos, &ypos);
@@ -20,24 +19,29 @@ Camera::Camera(GLFWwindow* window, glm::vec3 position) :
     m_LastY = (float)ypos;
     int width, height;
     glfwGetWindowSize(m_Window, &width, &height);
-    if (height > 0) {
+    if (height > 0)
+    {
         m_AspectRatio = (float)width / (float)height;
     }
     updateMatrices();
 }
 
-void Camera::HandleInput(float deltaTime, std::function<void()> onUpdateCallback) {
+void Camera::HandleInput(float deltaTime, std::function<void()> onUpdateCallback)
+{
     bool keyboardMoved = processKeyboard(deltaTime);
     bool mouseMoved = false;
 
-    if (glfwGetMouseButton(m_Window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
-        if (!m_IsOrbiting) {
+    if (glfwGetMouseButton(m_Window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+    {
+        if (!m_IsOrbiting)
+        {
             m_IsOrbiting = true;
             m_FirstMouse = true;
         }
         double xpos, ypos;
         glfwGetCursorPos(m_Window, &xpos, &ypos);
-        if (m_FirstMouse) {
+        if (m_FirstMouse)
+        {
             m_LastX = (float)xpos;
             m_LastY = (float)ypos;
             m_FirstMouse = false;
@@ -47,32 +51,39 @@ void Camera::HandleInput(float deltaTime, std::function<void()> onUpdateCallback
         m_LastX = (float)xpos;
         m_LastY = (float)ypos;
 
-        if (xoffset != 0.0f || yoffset != 0.0f) {
+        if (xoffset != 0.0f || yoffset != 0.0f)
+        {
             mouseMoved = processMouseMovement(xoffset, yoffset);
         }
-    } else {
+    }
+    else
+    {
         m_IsOrbiting = false;
     }
-    
-    if ((keyboardMoved || mouseMoved) && onUpdateCallback) {
+
+    if ((keyboardMoved || mouseMoved) && onUpdateCallback)
+    {
         onUpdateCallback();
     }
 }
 
-void Camera::ProcessMouseScroll(float yoffset) {
+void Camera::ProcessMouseScroll(float yoffset)
+{
     m_Position += m_Front * yoffset * 0.5f;
     updateMatrices();
 }
 
-void Camera::SetAspectRatio(float aspectRatio) {
-    if (m_AspectRatio != aspectRatio) {
+void Camera::SetAspectRatio(float aspectRatio)
+{
+    if (m_AspectRatio != aspectRatio)
+    {
         m_AspectRatio = aspectRatio;
         updateMatrices();
     }
 }
 
-// --- NEW METHOD IMPLEMENTATION ---
-glm::vec3 Camera::WorldToScreenDirection(const glm::vec3& worldDir) const {
+glm::vec3 Camera::WorldToScreenDirection(const glm::vec3 &worldDir) const
+{
     glm::mat4 vp = m_ProjectionMatrix * m_ViewMatrix;
 
     // Use two points to define the direction vector in world space
@@ -94,8 +105,8 @@ glm::vec3 Camera::WorldToScreenDirection(const glm::vec3& worldDir) const {
     return screenDir;
 }
 
-
-void Camera::updateMatrices() {
+void Camera::updateMatrices()
+{
     glm::vec3 front;
     front.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
     front.y = sin(glm::radians(m_Pitch));
@@ -107,34 +118,41 @@ void Camera::updateMatrices() {
     m_ProjectionMatrix = glm::perspective(glm::radians(m_Zoom), m_AspectRatio, 0.1f, 100.0f);
 }
 
-bool Camera::processKeyboard(float deltaTime) {
+bool Camera::processKeyboard(float deltaTime)
+{
     float velocity = m_MovementSpeed * deltaTime;
     bool moved = false;
 
-    if (glfwGetKey(m_Window, GLFW_KEY_W) == GLFW_PRESS) {
+    if (glfwGetKey(m_Window, GLFW_KEY_W) == GLFW_PRESS)
+    {
         m_Position += m_Front * velocity;
         moved = true;
     }
-    if (glfwGetKey(m_Window, GLFW_KEY_S) == GLFW_PRESS) {
+    if (glfwGetKey(m_Window, GLFW_KEY_S) == GLFW_PRESS)
+    {
         m_Position -= m_Front * velocity;
         moved = true;
     }
-    if (glfwGetKey(m_Window, GLFW_KEY_A) == GLFW_PRESS) {
+    if (glfwGetKey(m_Window, GLFW_KEY_A) == GLFW_PRESS)
+    {
         m_Position -= m_Right * velocity;
         moved = true;
     }
-    if (glfwGetKey(m_Window, GLFW_KEY_D) == GLFW_PRESS) {
+    if (glfwGetKey(m_Window, GLFW_KEY_D) == GLFW_PRESS)
+    {
         m_Position += m_Right * velocity;
         moved = true;
     }
-    
-    if (moved) {
+
+    if (moved)
+    {
         updateMatrices();
     }
     return moved;
 }
 
-bool Camera::processMouseMovement(float xoffset, float yoffset) {
+bool Camera::processMouseMovement(float xoffset, float yoffset)
+{
     float sensitivity = 0.1f;
     xoffset *= sensitivity;
     yoffset *= sensitivity;
@@ -142,8 +160,10 @@ bool Camera::processMouseMovement(float xoffset, float yoffset) {
     m_Yaw += xoffset;
     m_Pitch += yoffset;
 
-    if (m_Pitch > 89.0f) m_Pitch = 89.0f;
-    if (m_Pitch < -89.0f) m_Pitch = -89.0f;
+    if (m_Pitch > 89.0f)
+        m_Pitch = 89.0f;
+    if (m_Pitch < -89.0f)
+        m_Pitch = -89.0f;
 
     updateMatrices();
     return true;
--- src/Scene/Objects/BaseObject.cpp
+++ src/Scene/Objects/BaseObject.cpp
@@ -2,9 +2,10 @@
 #include "Shader.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/matrix_decompose.hpp> 
+#include <glm/gtx/matrix_decompose.hpp>
 
-BaseObject::BaseObject() {
+BaseObject::BaseObject()
+{
     // Generate GL buffers & arrays
     glGenVertexArrays(1, &m_VAO);
     glGenBuffers(1, &m_VBO);
@@ -12,25 +13,31 @@ BaseObject::BaseObject() {
 
     // Default shader
     m_Shader = std::make_unique<Shader>(
-        "shaders/default.vert", "shaders/default.frag"
-    );
+        "shaders/default.vert", "shaders/default.frag");
 }
 
-BaseObject::~BaseObject() {
-    if (m_EBO) glDeleteBuffers(1, &m_EBO);
-    if (m_VBO) glDeleteBuffers(1, &m_VBO);
-    if (m_VAO) glDeleteVertexArrays(1, &m_VAO);
+BaseObject::~BaseObject()
+{
+    if (m_EBO)
+        glDeleteBuffers(1, &m_EBO);
+    if (m_VBO)
+        glDeleteBuffers(1, &m_VBO);
+    if (m_VAO)
+        glDeleteVertexArrays(1, &m_VAO);
 }
 
-glm::vec3 BaseObject::GetPosition() const {
+glm::vec3 BaseObject::GetPosition() const
+{
     return glm::vec3(transform[3]);
 }
 
-void BaseObject::SetPosition(const glm::vec3& position) {
+void BaseObject::SetPosition(const glm::vec3 &position)
+{
     transform[3] = glm::vec4(position, 1.0f);
 }
 
-glm::vec3 BaseObject::GetEulerAngles() const {
+glm::vec3 BaseObject::GetEulerAngles() const
+{
     glm::vec3 scale, skew, position;
     glm::quat rotation;
     glm::vec4 perspective;
@@ -47,28 +54,29 @@ void BaseObject::SetupMesh(const std::vector<float> &vertices,
 
     glBindVertexArray(m_VAO);
 
-      glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-      glBufferData(GL_ARRAY_BUFFER,
-                   vertices.size() * sizeof(float),
-                   vertices.data(),
-                   GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    glBufferData(GL_ARRAY_BUFFER,
+                 vertices.size() * sizeof(float),
+                 vertices.data(),
+                 GL_STATIC_DRAW);
 
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-      glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                   indices.size() * sizeof(unsigned int),
-                   indices.data(),
-                   GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                 indices.size() * sizeof(unsigned int),
+                 indices.data(),
+                 GL_STATIC_DRAW);
 
-      // position attribute
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
-                            3 * sizeof(float),
-                            (void*)0);
-      glEnableVertexAttribArray(0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                          3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
 }
 
-void BaseObject::RebuildMesh() {
+void BaseObject::RebuildMesh()
+{
     std::vector<float> verts;
     std::vector<unsigned int> inds;
     BuildMeshData(verts, inds);
@@ -79,8 +87,8 @@ void BaseObject::Draw(const glm::mat4 &view,
                       const glm::mat4 &projection)
 {
     m_Shader->Bind();
-    m_Shader->SetUniformMat4f("u_Model",      transform);
-    m_Shader->SetUniformMat4f("u_View",       view);
+    m_Shader->SetUniformMat4f("u_Model", transform);
+    m_Shader->SetUniformMat4f("u_View", view);
     m_Shader->SetUniformMat4f("u_Projection", projection);
     m_Shader->SetUniform4f("u_Color",
                            m_Color.x, m_Color.y,
@@ -97,10 +105,10 @@ void BaseObject::DrawForPicking(Shader &pickingShader,
                                 const glm::mat4 &projection)
 {
     pickingShader.Bind();
-    pickingShader.SetUniformMat4f("u_Model",      transform);
-    pickingShader.SetUniformMat4f("u_View",       view);
+    pickingShader.SetUniformMat4f("u_Model", transform);
+    pickingShader.SetUniformMat4f("u_View", view);
     pickingShader.SetUniformMat4f("u_Projection", projection);
-    pickingShader.SetUniform1ui("u_ObjectID",    id);
+    pickingShader.SetUniform1ui("u_ObjectID", id);
 
     glBindVertexArray(m_VAO);
     glDrawElements(GL_TRIANGLES, m_IndexCount,
@@ -118,14 +126,14 @@ void BaseObject::DrawHighlight(const glm::mat4 &view,
     glBindVertexArray(0);
 }
 
-std::vector<ObjectProperty> BaseObject::GetProperties() {
+std::vector<ObjectProperty> BaseObject::GetProperties()
+{
     return {
-        { "Width",  &m_Width  },
-        { "Height", &m_Height },
-        { "Depth",  &m_Depth  },
-        { "Color R",&m_Color.r },
-        { "Color G",&m_Color.g },
-        { "Color B",&m_Color.b },
-        { "Color A",&m_Color.a }
-    };
+        {"Width", &m_Width},
+        {"Height", &m_Height},
+        {"Depth", &m_Depth},
+        {"Color R", &m_Color.r},
+        {"Color G", &m_Color.g},
+        {"Color B", &m_Color.b},
+        {"Color A", &m_Color.a}};
 }
